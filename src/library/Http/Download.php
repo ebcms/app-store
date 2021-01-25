6be32d7f7dba1e1cf417bc09eedf1be1d53e5a3f
ebcms <1540837821@qq.com>
2021-01-25 18:12:58 +0800

    1

--- src/library/Http/Download.php
+++ src/library/Http/Download.php
@@ -6,27 +6,26 @@ namespace App\Ebcms\Store\Http;
 
 use App\Ebcms\Admin\Http\Common;
 use App\Ebcms\Store\Traits\DirTrait;
-use Ebcms\App;
 use Ebcms\Session;
+use Throwable;
 
 class Download extends Common
 {
     use DirTrait;
 
     public function get(
-        App $app,
         Session $session
     ) {
         $plugin = $session->get('plugin');
         try {
             $content = file_get_contents($plugin['source']);
-        } catch (\Throwable $th) {
+        } catch (Throwable $th) {
             return $this->failure('文件下载失败！');
         }
         if (md5($content) != $plugin['md5']) {
             return $this->failure('文件校验失败！');
         }
-        $tmpfile = $app->getAppPath() . '/runtime/' . 'plugin_' . $plugin['md5'] . '.tmp';
+        $tmpfile = tempnam(sys_get_temp_dir(), 'storeinstall');
         file_put_contents($tmpfile, $content);
         $plugin['tmpfile'] = $tmpfile;
         $session->set('plugin', $plugin);
