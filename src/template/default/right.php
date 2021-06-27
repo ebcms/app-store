5371f104fb9c25841ee944ddaa250304ba75d3f4
ebcms <1540837821@qq.com>
2021-06-27 19:17:53 +0800

    1

--- src/template/default/right.php
+++ src/template/default/right.php
@@ -26,16 +26,20 @@
 <div class="mb-3">
     <?php $res = $server->query('/good'); ?>
     {if $res['status']==200}
-    <ul class="list-unstyled list-inline">
-        {foreach $res['data'] as $vo}
-        <li class="list-inline-item text-center">
-            <a href="{:$router->buildUrl('/ebcms/store/detail',['plugin_name'=>$vo['name']])}">
-                <img src="{$vo['logo']??''}" alt="..." class="img-thumbnail p-2" style="width:80px;height:80px;">
-                <div class="mt-2 mb-3">{$vo['title']??$vo['name']}</div>
-            </a>
-        </li>
-        {/foreach}
-    </ul>
+    {foreach $res['data'] as $vo}
+    <div class="d-flex mb-2 p-1 plugin-item position-relative">
+        <div class="me-2">
+            <img src="{$vo['logo']??''}" alt="..." class="img-thumbnail p-2" style="width:80px;height:80px;">
+        </div>
+        <div class="flex-fill">
+            <div class="mt-0 mb-1"><strong>{$vo['title']??$name}</strong></div>
+            <div class="text-muted">{$vo['description']??'暂无介绍'}</div>
+            <div>
+                <a class="text-decoration-none stretched-link" href="{:$router->buildUrl('/ebcms/store/detail',['plugin_name'=>$vo['name']])}">详情</a>
+            </div>
+        </div>
+    </div>
+    {/foreach}
     {else}
     <div class="alert alert-warning">{$res['message']??'接口出错，请稍后再试~'}</div>
     {/if}
@@ -47,16 +51,20 @@
 <div class="mb-3">
     <?php $res = $server->query('/newest'); ?>
     {if $res['status']==200}
-    <ul class="list-unstyled list-inline">
-        {foreach $res['data'] as $vo}
-        <li class="list-inline-item text-center">
-            <a href="{:$router->buildUrl('/ebcms/store/detail',['plugin_name'=>$vo['name']])}">
-                <img src="{$vo['logo']??''}" alt="..." class="img-thumbnail p-2" style="width:80px;height:80px;">
-                <div class="mt-2 mb-3">{$vo['title']??$vo['name']}</div>
-            </a>
-        </li>
-        {/foreach}
-    </ul>
+    {foreach $res['data'] as $vo}
+    <div class="d-flex mb-2 p-1 plugin-item position-relative">
+        <div class="me-2">
+            <img src="{$vo['logo']??''}" alt="..." class="img-thumbnail p-2" style="width:80px;height:80px;">
+        </div>
+        <div class="flex-fill">
+            <div class="mt-0 mb-1"><strong>{$vo['title']??$name}</strong></div>
+            <div class="text-muted">{$vo['description']??'暂无介绍'}</div>
+            <div>
+                <a class="text-decoration-none stretched-link" href="{:$router->buildUrl('/ebcms/store/detail',['plugin_name'=>$vo['name']])}">详情</a>
+            </div>
+        </div>
+    </div>
+    {/foreach}
     {else}
     <div class="alert alert-warning">{$res['message']??'接口出错，请稍后再试~'}</div>
     {/if}
